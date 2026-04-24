7172da3ef0f1206db74efe0e723b0cb986d78490
shPark <pjszzal@naver.com>
2026-04-24 10:38:41 +0900

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    public void sayHello() {
+        System.out.println("Hello from Demo1Application!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public String getStatus() {
+        return "Application is running";
+    }
+}
+
+
